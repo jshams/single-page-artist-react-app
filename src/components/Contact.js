cd71325a91c0054ceb08cc787b120c9dc6dcd95c
Jake Shams <jshams62@gmail.com>
2019-05-07 11:08:45 -0700

    added contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = (props) => {
+    return (
+        <div>
+            <input placeholder="Name"/>
+            <textarea placeholder="message"/>
+            <button>send</button>
+        </div>
+    )
+}
+
+// REFERENCE INPUTS IN WEATHER APP OR INCREMENTOR CLASS LESSON
+
+export default Contact
