8d831f0e426891ff6fa269587bde57bb5077e527
Jake Shams <jshams62@gmail.com>
2019-05-07 11:04:35 -0700

    Events component no longer undefined

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
 import About from './components/About'
 import Songs from './components/Songs'
+import Events from './components/Events'
 
 const Home = (props) => {
   return (
