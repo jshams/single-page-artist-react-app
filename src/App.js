4a6d33b01ddd418aa457b33203f6616b708901d5
Jake Shams <jshams62@gmail.com>
2019-05-07 10:16:55 -0700

    added routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/songs">Songs</Link>
+          </li>
+          <li>
+            <Link to="/events">Events</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/songs" component={Songs} />
+        <Route path="/events" component={Events} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </BrowserRouter>
   );
 }
 
