37d0ffabe6c110bef433686e6cb1e2689cd982c3
Jake Shams <jshams62@gmail.com>
2019-05-08 16:35:33 -0700

    new wireframe for contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,9 @@ import React from 'react'
 const Contact = (props) => {
     return (
         <div>
-            <input placeholder="Name"/>
-            <textarea placeholder="message"/>
-            <button>send</button>
+            <a className="email" href={'mailto:' + props.email}>{props.name}</a>
         </div>
     )
 }
-
-// REFERENCE INPUTS IN WEATHER APP OR INCREMENTOR CLASS LESSON
 
 export default Contact
