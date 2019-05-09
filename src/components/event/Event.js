7dd66e142a5e771374e234dd986d1406144db465
Jake Shams <jshams62@gmail.com>
2019-05-08 17:19:26 -0700

    reconstructing file architecture

---
+++ src/components/event/Event.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Event.css'
+
+const Event = (props) => {
+    return (
+        <div className="Event">
+            <h3 className="Date">{props.date}</h3>
+            <h3 className="Location">{props.location}</h3>
+            <button className="btn-right">Tickets</button>
+        </div>
+    )
+}
+
+export default Event
---
+++ src/components/event/Events.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Event from './Event'
+import './Event.css'
+
+const Events = (props) => {
+    const events = props.events.map((event) => {
+        return (
+            <Event
+                date={props.date}
+                location={props.location}
+            />
+        )
+      })
+
+    return (
+        <div className="Events">
+            {events}
+        </div>
+    )
+}
+
+export default Events
