bde616daaa95040b8ab5c7623ee73dc9eccee83b
Jake Shams <jshams62@gmail.com>
2019-05-09 10:14:20 -0700

    added map function to handle array input

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,14 +3,53 @@ import AboutImgLeft from './AboutImgLeft'
 import AboutNoImg from './AboutNoImg'
 import AboutImgRight from './AboutImgRight'
 import './About.css'
+// props takes in an array of objects
+// objects should look like this:
+// section = { backgroung: "red"
+//             heading: "About me"
+//             text: "long paragraph"
+//             img: "https://facebook.com" }
 
 const About = (props) => {
-    // do something with props
+    const abouts = props.section.map((section, i) => {
+        if (i % 3 === 0) {
+            return (
+                <AboutImgRight
+                background = {section.background}
+                heading = {section.heading}
+                text = {section.text}
+                img = {section.img}
+                />
+            )
+        }
+        else if (i % 3 === 1) {
+            if (section.img != null) {
+                console.log(`${section.heading} has a photo, but due to its placement it wont display`)
+            }
+            return (
+                <AboutNoImg
+                background = {section.background}
+                heading = {section.heading}
+                text = {section.test}
+                />
+                )
+        }
+        // if i % 3 == 2
+        else {
+            return (
+                <AboutImgLeft
+                background = {section.background}
+                heading = {section.heading}
+                text = {section.text}
+                img = {section.img}
+                />
+            )
+        }
+    })
+
     return (
         <div className="about">
-            <AboutImgLeft/>
-            <AboutImgRight/>
-            <AboutNoImg/>
+            {abouts}
         </div>
     )
 }
