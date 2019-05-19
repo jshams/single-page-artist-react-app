ec5a3091169185fd68acb7b405be7a2ede011742
Jake Shams <jshams62@gmail.com>
2019-05-19 12:48:21 -0400

    put img and p tag in one div

--- src/components/about/AboutImgLeft.js
+++ src/components/about/AboutImgLeft.js
@@ -5,10 +5,10 @@ const AboutImgLeft = (props) => {
     return (
         <div className={props.backgroung}>
             <h1 className="header">{props.heading}</h1>
-            <div className="text-right">
-                {props.text}
+            <div className="content-right">
+                <p className="text-right">{props.text}</p>
+                <img src={props.img} alt={props.heading} className="img-left" />
             </div>
-            <img src={props.img} alt={props.heading} className="img-left" />
         </div>
     )
 }
--- src/components/about/AboutImgRight.js
+++ src/components/about/AboutImgRight.js
@@ -5,10 +5,10 @@ const AboutImgRight = (props) => {
     return (
         <div className={props.backgroung}>
             <h1 className="header">{props.heading}</h1>
-            <div className="text-left">
-                {props.text}
+            <div className="content-left">
+                <p className="text-left">{props.text}</p>
+                <img src={props.img} alt={props.heading} className="img-right" />
             </div>
-            <img src={props.img} alt={props.heading} className="img-right" />
         </div>
     )
 }
