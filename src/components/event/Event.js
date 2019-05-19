fe5ae49858fda07d27b10ada85e4da81f37ada01
Jake Shams <jshams62@gmail.com>
2019-05-18 23:24:36 -0700

    renamed classnames

--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -3,9 +3,9 @@ import './Event.css'
 
 const Event = (props) => {
     return (
-        <div className="Event">
-            <h3 className="Date">{props.date}</h3>
-            <h3 className="Location">{props.location}</h3>
+        <div className="event">
+            <h3 className="date">{props.date}</h3>
+            <h3 className="location">{props.location}</h3>
             <button className="btn-right">Tickets</button>
         </div>
     )
